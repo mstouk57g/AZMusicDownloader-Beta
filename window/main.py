8cfa1b27aee742e8b2f7ca4c6736ffe5e2e01c59
mstouk57g <KissLmisu@outlook.com>
2024-05-01 00:46:01 +0800

    精简部分代码

--- window/main.py
+++ window/main.py
@@ -30,27 +30,19 @@ class Window(MSFluentWindow):
     def __init__(self):
         super().__init__()
 
-        # create sub interface
-        self.searchInterface = searchmusic()
-        self.localmusics = Demo()
-        self.releases = note()
-        self.playlist = playlist()
-        self.betatip_playtip = playlist_tip()
-        self.settingsInterface = SettingInterface()
-
         self.initNavigation()
         self.initWindow()
 
     def initNavigation(self):
-        self.addSubInterface(self.searchInterface, FIF.CARE_RIGHT_SOLID, '搜索下载')
-        self.addSubInterface(self.localmusics, FIF.MUSIC_FOLDER, '我的音乐库')
+        self.addSubInterface(searchmusic(), FIF.CARE_RIGHT_SOLID, '搜索下载')
+        self.addSubInterface(Demo(), FIF.MUSIC_FOLDER, '我的音乐库')
         if helper.config.Config.beta.value == True:
-            self.addSubInterface(self.playlist, FIF.EXPRESSIVE_INPUT_ENTRY, '歌单')
+            self.addSubInterface(playlist(), FIF.EXPRESSIVE_INPUT_ENTRY, '歌单')
         else:
-            self.addSubInterface(self.betatip_playtip, FIF.EXPRESSIVE_INPUT_ENTRY, '歌单')
+            self.addSubInterface(playlist_tip(), FIF.EXPRESSIVE_INPUT_ENTRY, '歌单')
         #self.addSubInterface(web(), FIF.GLOBE, 'WEB管理', NavigationItemPosition.BOTTOM)
         self.addSubInterface(self.releases, FIF.QUICK_NOTE, '更新日志', position=NavigationItemPosition.BOTTOM)
-        self.addSubInterface(self.settingsInterface, FIF.SETTING, '设置', position=NavigationItemPosition.BOTTOM)
+        self.addSubInterface(SettingInterface(), FIF.SETTING, '设置', position=NavigationItemPosition.BOTTOM)
 
         self.navigationInterface.setCurrentItem(self.searchInterface.objectName())
 
