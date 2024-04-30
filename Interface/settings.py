151da28364d59340139205f481e47478d3afde2a
mstouk57g <KissLmisu@outlook.com>
2024-05-01 01:22:08 +0800

    修复重置失败的问题

--- Interface/settings.py
+++ Interface/settings.py
@@ -230,7 +230,11 @@ class SettingInterface(ScrollArea):
         
     def __backtoinitClicked(self):
         try:
-            remove("config/config.json")
+            reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
+            config_path_value = winreg.QueryValueEx(reg_key, "AppData")
+            DataPath = config_path_value[0]
+            configpath = "{}\\AZMusicDownload\\config.json".format(DataPath)
+            remove(configpath)
             self.__showRestartTooltip()
             self.backtoinit.setEnabled(False)
         except:
