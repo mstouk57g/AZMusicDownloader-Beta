a8d00c9c318c92a8cfdc225f1ec0824f600c2a0b
mstouk57g <KissLmisu@outlook.com>
2024-05-05 11:14:19 +0800

    加了一个全局异常捕获

--- MusicDownloader.py
+++ MusicDownloader.py
@@ -2,16 +2,24 @@ from PyQt5.QtWidgets import QApplication, QSplashScreen
 from qfluentwidgets import FluentTranslator
 from helper.config import cfg
 from window.main import Window
-from sys import argv
+import sys
 from PyQt5.QtCore import Qt, QTranslator
 from PyQt5.QtGui import QPixmap
+import win32api
+import win32con
+
+def global_exception_handler(exc_type, exc_value, exc_traceback):
+    msesg = exc_type, exc_value, exc_traceback
+    win32api.MessageBox(0, msesg, "完犊子咯！这里捕获了异常：", win32con.MB_ICONHAND)
+    # 这个全局捕获把我电脑干废过（
+sys.excepthook = global_exception_handler
 
 if __name__ == '__main__':
     QApplication.setHighDpiScaleFactorRoundingPolicy(
         Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-    app = QApplication(argv)
+    app = QApplication(sys.argv)
     
     splash_pix = QPixmap('resource/splash.png')
     splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
@@ -32,6 +40,7 @@ if __name__ == '__main__':
     app.installTranslator(fluentTranslator)
     app.installTranslator(settingTranslator)
     app.processEvents()
+    
     w = Window()
     w.show()
     splash.finish(w)
